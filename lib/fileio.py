32791c21f1f27ef71f888b4a380e358746207103
ReineoMesti <reineomesti@gmail.com>
2024-08-06 17:19:06 +0800

    Create fileio.py

---
+++ lib/fileio.py
@@ -0,0 +1,17 @@
+import os
+import copy
+to_import = {"debug":"dbg"}
+for module, newname in to_import.items():
+    try:
+        exec('import '+module+' as '+newname)
+    except ModuleNotFoundError:
+        exec('import lib.'+module+' as '+newname)
+
+def get_formula(filename):
+    file = open('lib/data/'+filename)
+    content = file.read()
+    content = content.replace('\n', '')
+    content = content.replace('\r', '')
+    content = content.replace('\t', '')
+    output = eval(content)
+    return copy.deepcopy(output)
