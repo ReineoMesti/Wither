797caca163bb6588a5b07c39087e5e17e2fee9e3
ReineoMesti <reineomesti@gmail.com>
2024-08-08 09:07:39 +0800

    Update display.py

--- lib/display.py
+++ lib/display.py
@@ -12,36 +12,13 @@ class display_page:
     def __init__(self, master, text):
         self.page = tk.LabelFrame(master, text=text)
     def grid(self, **kw):
-        command = 'self.page.grid('
-        for key, val in kw.items():
-            command += key + '=\'' + str(val) + '\','
-        if command[-1] == ',':
-            command = command[:-1]
-        command += ')'
-        exec(command)
+        self.page.grid(**kw)
     def pack(self, **kw):
-        command = 'self.page.pack('
-        for key, val in kw.items():
-            command += key + '=\'' + str(val) + '\','
-        if command[-1] == ',':
-            command = command[:-1]
-        command += ')'
-        exec(command)
+        self.page.pack(**kw)
     def destroy(self):
         self.page.destroy()
     def config(self, **kw):
-        # It's a pity these arguments cannot be passed
-        #   directly to self.page.config, at least, well,
-        #   I have never heard of skills for that.
-        # But if you know how to do, please rewrite this function.
-        # Lots of thanks.
-        command = 'self.page.config('
-        for key, val in kw.items():
-            command += key + '=\'' + str(val) + '\','
-        if command[-1] == ',':
-            command = command[:-1]
-        command += ')'
-        exec(command)
+        self.page.config(**kw)
     def __del__(self):
         self.page.destroy()
 class child_window:
