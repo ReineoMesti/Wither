f7290043c82855e9e3b91b262a1c7c3db91d6f60
ReineoMesti <reineomesti@gmail.com>
2024-08-08 09:31:06 +0800

    Update expandrandom.py

--- lib/expandrandom.py
+++ lib/expandrandom.py
@@ -1,5 +1,15 @@
 import random as rand
-def linear_uneven(term, lower, upper, integer=True):
+
+unif = rand.uniform
+randi = rand.randint
+def coord_hash(mod, **kwarg):
+    res = 1
+    for key, val in kwarg.items():
+        res *= (hash(key + '=' + str(val)) + 1)
+        res %= mod
+    return res
+
+def polynominal_linear(term, lower, upper, integer=True):
     low_limit = lower / term
     up_limit = upper / term
     result = 0
@@ -10,3 +20,12 @@ def linear_uneven(term, lower, upper, integer=True):
     result = min(result, upper)
     result = max(result, lower)
     return result
+def weighted_choice(options:dict, rengine = unif):
+    'Choice by weighted chance'
+    w_range = sum(options.values())
+    lottery = rengine(0, w_range)
+    for key, val in options.items():
+        if w_range <= val:
+            return key
+        w_range -= val
+    return key
