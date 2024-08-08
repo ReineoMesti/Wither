51aac56b37e3536f4c13424c8ccfaf4ba2fb87b8
ReineoMesti <reineomesti@gmail.com>
2024-08-08 09:08:08 +0800

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,19 +1,12 @@
 import os
 import time
-if __name__ in ('__main__', 'debug'):
-    os.chdir('../log')
-    log_output = open('logout.log', mode='a+')
-    error_output = open('error.log', mode='a+')
-else:
-    print(__name__)
-    log_output = open('log/logout.log', mode='a+')
-    error_output = open('log/error.log', mode='a+')
+
+import threading as thread
+from tkinter import messagebox as msgbox
+
 start_time = time.time()
-def report(source, message):
-    global error_output
-    running_time = int(time.time() - start_time) 
-    error_output.write('[%d] %s: %s\n'%(running_time, source, message))
-def log(source, message):
-    global log_output
-    running_time = int(time.time() - start_time) 
-    log_output.write('[%d] %s: %s\n'%(running_time, source, message))
+
+def error(source:str, message:str)->None:
+    msgbox.showerror("Error from "+source, message)
+def log(source:str, message:str)->None:
+    msgbox.showinfo("Log message from "+source, message)
